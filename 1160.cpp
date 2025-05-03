ea645805e8baa3f876514baade4c9e6015849516
nsukhov <nsukhov22@gmail.com>
2025-05-03 16:54:32 +0300

    1160 done

--- 1160.cpp
+++ 1160.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
-struct Edge {
+struct edge {
     int u, v, length;
 };
 
-bool compareEdges(const Edge &a, const Edge &b);
-void unionSets(int u, int v, std::vector<int>* parent);
-int find(int u, std::vector<int>* parent);
+bool edgecomparer(const edge &a, const edge &b);
+void unionSets(int u, int v, int* parent);
+int find(int u, int* parent);
+int Partition(std::vector<edge>& A, int p, int r);
+void Quicksort(std::vector<edge>& A, int p, int r);
+
 
 int main() {
     std::ios_base::sync_with_stdio(false);
@@ -16,55 +18,77 @@ int main() {
 
     int N, M;
     std::cin >> N >> M;
-    std::vector<int> parent;
-
-    std::vector<Edge> edges(M);
+    int parent[N + 1];
+    std::vector<edge> edges(M);
     for (int i = 0; i < M; ++i) {
         std::cin >> edges[i].u >> edges[i].v >> edges[i].length;
     }
 
-    std::sort(edges.begin(), edges.end(), compareEdges);
+    Quicksort(edges, 0, M - 1);
 
-    parent.resize(N + 1);
     for (int i = 1; i <= N; ++i) {
         parent[i] = i;
     }
 
-    std::vector<std::pair<int, int>> mstEdges;
+    std::vector<std::pair<int, int>> mstedges;
     int maxLen = 0;
 
-    for (const Edge &edge : edges) {
-        if (find(edge.u, &parent) != find(edge.v, &parent)) {
-            unionSets(edge.u, edge.v, &parent);
-            mstEdges.emplace_back(edge.u, edge.v);
-            maxLen = edge.length;
+    for (const edge &e : edges) {
+        if (find(e.u, parent) != find(e.v, parent)) {
+            unionSets(e.u, e.v, parent);
+            mstedges.emplace_back(e.u, e.v);
+            maxLen = e.length;
         }
     }
 
-    std::cout << maxLen << '\n';
-    std::cout << mstEdges.size() << '\n';
-    for (const auto &edge : mstEdges) {
-        std::cout << edge.first << ' ' << edge.second << '\n';
+    std::cout << maxLen << std::endl << mstedges.size() << std::endl;
+    for (const std::pair<int, int> &e : mstedges) {
+        std::cout << e.first << ' ' << e.second << std::endl;
     }
 
     return 0;
 }
 
-bool compareEdges(const Edge &a, const Edge &b) {
+bool edgecomparer(const edge &a, const edge &b) {
     return a.length < b.length;
 }
 
-int find(int u, std::vector<int>* parent) {
-    if ((*parent)[u] != u) {
-        (*parent)[u] = find((*parent)[u], parent);
+int find(int u, int* parent) {
+    if (parent[u] != u) {
+        parent[u] = find(parent[u], parent);
     }
-    return (*parent)[u];
+    return parent[u];
 }
 
-void unionSets(int u, int v, std::vector<int>* parent) {
+void unionSets(int u, int v, int* parent) {
     int rootU = find(u, parent);
     int rootV = find(v, parent);
     if (rootU != rootV) {
-        (*parent)[rootV] = rootU;
+        parent[rootV] = rootU;
+    }
+}
+
+int Partition(std::vector<edge>& A, int p, int r) {
+    edge x = A[r];
+    int i = p - 1;
+    for (int j = p; j < r; ++j)
+        if (edgecomparer(A[j], x)) {
+            ++i;
+            edge temp = A[i];
+            A[i] = A[j];
+            A[j] = temp;
+        }
+    ++i;
+    edge temp = A[i];
+    A[i] = A[r];
+    A[r] = temp;
+    return i;
+}
+
+void Quicksort(std::vector<edge>& A, int p, int r) {
+    if (p < r) {
+        int q = Partition(A, p, r);
+        Quicksort(A, p, q - 1);
+        Quicksort(A, q + 1, r);
     }
 }
