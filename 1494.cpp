ed051a5c41e54f23c8778a74942484b31dc89e1e
nsukhov <nsukhov22@gmail.com>
2025-05-14 12:15:07 +0300

    1494 done

--- 1494.cpp
+++ 1494.cpp
@@ -1,7 +1,43 @@
 #include <iostream>
+#include <vector>
+#include <stack>
 
-int main(int argc, char const *argv[])
-{
-    
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int n;
+    std::cin >> n;
+
+    std::vector<int> sequence(n);
+    for (int i = 0; i < n; ++i) {
+        std::cin >> sequence[i];
+    }
+
+    std::stack<int> stk;
+    int current = 1;
+    int idx = 0;
+
+    while (current <= n) {
+        stk.push(current);
+        while (!stk.empty() && idx < n && stk.top() == sequence[idx]) {
+            stk.pop();
+            ++idx;
+        }
+        ++current;
+    }
+
+    while (!stk.empty() && idx < n && stk.top() == sequence[idx]) {
+        stk.pop();
+        ++idx;
+    }
+
+    if (idx == n) {
+        std::cout << "Not a proof";
+    }
+    else {
+        std::cout << "Cheater";
+    }
+
     return 0;
 }
