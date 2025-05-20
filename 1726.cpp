d9df7286fd7051548001d759e23218c10d6de425
nsukhov <nsukhov22@gmail.com>
2025-05-20 03:39:33 +0300

    1726 done

--- 1726.cpp
+++ 1726.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
-int main(int argc, char const *argv[])
-{
+unsigned long long count_distance(std::vector<int>& coords) {
+    sort(coords.begin(), coords.end());
+    unsigned long long total = 0;
+    unsigned long long prefixSum = 0;
+    for (int i = 0; i < coords.size(); ++i) {
+        total += (unsigned long long)coords[i] * i - prefixSum;
+        prefixSum += coords[i];
+    }
+    return total;
+}
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    
+    int n;
+    std::cin >> n;
+    std::vector<int> x(n), y(n);
+    for (int i = 0; i < n; ++i)
+        std::cin >> x[i] >> y[i];
+    
+    unsigned long long sumX = count_distance(x);
+    unsigned long long sumY = count_distance(y);
+    
+    unsigned long long totalSum = sumX + sumY;
+    unsigned long long average = totalSum / (n * (n - 1LL) / 2LL);
+    
+    std::cout << average;
     
     return 0;
 }
